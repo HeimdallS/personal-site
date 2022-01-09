7a9dffb3006102b618824dcbc957d55573f1fdcd
Heimdall Sham <heim.sham@icloud.com>
2022-01-08 20:23:31 -0500

    Created homepage. Added templates.

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <main className="content">
+            <h2 className="content-mission">Mission</h2>
+            <p className="content__mission-statement">
+                This is where the mission statement will go.
+            </p>
+            
+        </main>
+    )
+}
