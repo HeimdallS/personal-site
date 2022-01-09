ef7b192c3c6b6ecaf2c4ebf4d2e4a9d2dbc06768
Heimdall Sham <heim.sham@icloud.com>
2022-01-08 22:18:38 -0500

    Applied styling to icons.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,35 +2,20 @@ import React from 'react'
 import * as BsIcon from 'react-icons/bs';
 import * as AiIcon from 'react-icons/ai';
 import './Header.scss';
-import {Link} from 'react-router-dom';
 
 export default function Header() {
-
-    // const mailIcon = ({mailto, label}) => {
-    //     return (
-    //         <Link
-    //             to='#'
-    //             onClick = {(e) => {
-    //                 e.preventDefault()
-    //                 window.location = mailto
-    //             }}
-    //         >
-    //             {label}
-    //         </Link>
-    //     )
-    // }
 
     return (
         <header>
             <h1>Heimdall Sham</h1>
             <div className="nav__icons">
-                <a href="https://www.linkedin.com/in/heimdall-sham/">
+                <a href="https://www.linkedin.com/in/heimdall-sham/" className='nav__link'>
                     <BsIcon.BsLinkedin className='nav__icon' href="https://www.linkedin.com/feed/"/>
                 </a>
-                <a href="https://github.com/HeimdallS">
+                <a href="https://github.com/HeimdallS" className='nav__link'>
                     <BsIcon.BsGithub className='nav__icon'/>
                 </a>
-                <a href="mailto:sham.heim@gmail.com">
+                <a href="mailto:sham.heim@gmail.com" className='nav__link'>
                     <AiIcon.AiOutlineMail className='nav__icon'/>
                 </a>
             </div>
