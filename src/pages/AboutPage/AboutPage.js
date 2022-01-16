dab50eaaae20893a7b938ef7b1050d906a163b32
Heimdall Sham <heim.sham@icloud.com>
2022-01-16 01:33:11 -0500

    Created about page.

---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function AboutPage() {
+    return (
+        <>
+            <h2 className="about">About</h2> 
+            <p className="about__statement">
+                After working in sales for the majority of my professional life, I consider myself 
+                an expert in communication and relationship management. However, I have always prided 
+                myself as being a life-long learner in pursuit of continued personal growth and 
+                development. My motive in switching career paths from Sales to Web Development is to 
+                immerse myself in a constantly evolving industry; one that will allow me to utilize my 
+                communication skills to work in collaborative environments while also pushing me to 
+                challenge myself and continue to hone my programming skills and abilities.
+            </p> 
+        </>
+    )
+}
+
+export default AboutPage
