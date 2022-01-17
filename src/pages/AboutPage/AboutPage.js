dd8729a7b474be69876c688b01457f248ea4e6dc
Heimdall Sham <heim.sham@icloud.com>
2022-01-16 21:59:33 -0500

    Revised font styling.

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 function AboutPage() {
     return (
-        <>
-            <h2 className="about">About</h2> 
-            <p className="about__statement">
-                After working in sales for the majority of my professional life, I consider myself 
-                an expert in communication and relationship management. However, I have always prided 
-                myself as being a life-long learner in pursuit of continued personal growth and 
-                development. My motive in switching career paths from Sales to Web Development is to 
-                immerse myself in a constantly evolving industry; one that will allow me to utilize my 
-                communication skills to work in collaborative environments while also pushing me to 
-                challenge myself and continue to hone my programming skills and abilities.
-            </p> 
-        </>
+        <main>
+            <section className='about'>
+                <h2 className="about__subhead">About</h2> 
+                <p className="about__statement">
+                    After working in sales for the majority of my professional life, I consider myself 
+                    an expert in communication and relationship management. However, I have always prided 
+                    myself as being a life-long learner in pursuit of continued personal growth and 
+                    development. My motive in switching career paths from Sales to Web Development is to 
+                    immerse myself in a constantly evolving industry; one that will allow me to utilize my 
+                    communication skills to work in collaborative environments while also pushing me to 
+                    challenge myself and continue to hone my programming skills and abilities.
+                </p> 
+            </section>
+            <section className="about__skills">
+                <h2 className="about__skills-subhead">Skills</h2>
+                
+            </section>
+        </main>
     )
 }
 
