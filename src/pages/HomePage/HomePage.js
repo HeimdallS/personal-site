6708b81128237988a6faae0e40f94aef0a553404
Heimdall Sham <heim.sham@icloud.com>
2022-01-21 16:29:58 -0500

    Styling homepage tablet view complete.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -86,32 +86,35 @@ export default function HomePage() {
     }
 
     return (
-        <main className="content">
-            <div className="content__container">
-                <div className="content__text-container">
-                    <h2 className="content__subhead">Hi, my name's Heimdall</h2>
-                    <p className="content__personal-statement">
-                        Thanks for visiting my page. I'm a Full Stack Web Developer proficient with <span className='content__highlight'>HTML/CSS</span>, <span className='content__highlight'>JavaScript</span>, <span className='content__highlight'>React</span>, <span className='content__highlight'>Node</span>, and a multitidue of 
-                        other frameworks and libraries. Enjoy perusing my portfolio, and feel free to reach out and chat with me sometime.   
-                    </p>
-                    <div className="content__link-container">
-                        <a href="https://www.linkedin.com/in/heimdall-sham/" className='content__nav-link'>
-                        <BsIcon.BsLinkedin className='content__nav-icon'/>
-                        </a>
-                        <a href="https://github.com/HeimdallS" className='content__nav-link'>
-                            <BsIcon.BsGithub className='content__nav-icon'/>
-                        </a>
-                        <a href="mailto:sham.heim@gmail.com" className='content__nav-link'>
-                            <AiIcon.AiOutlineMail className='content__nav-icon'/>
-                        </a>
-                    </div>
-                </div>
-                <img src={stars} alt="star gazing" className='content__profile-pic' />
-            </div>
-            <Animation toggle={toggle}/>
-            <div className="content__toggle">
-                <SwitchUnstyled component={Root} onChange={handleClick} defaultChecked/> 
-            </div>
-        </main>
+      <>
+      <main className="content">
+        <div className="content__container">
+          <div className="content__text-container">
+              <h2 className="content__subhead">Hi, my name's Heimdall</h2>
+              <p className="content__personal-statement">
+                  Thanks for visiting my page. I'm a Full Stack Web Developer proficient with <span className='content__highlight'>HTML/CSS</span>, <span className='content__highlight'>JavaScript</span>, <span className='content__highlight'>React</span>, <span className='content__highlight'>Node</span>, and a multitidue of 
+                  other frameworks and libraries. Enjoy perusing my portfolio, and feel free to reach out and chat with me sometime.   
+              </p>
+              <div className="content__link-container">
+                  <a href="https://www.linkedin.com/in/heimdall-sham/" className='content__nav-link'>
+                  <BsIcon.BsLinkedin className='content__nav-icon'/>
+                  </a>
+                  <a href="https://github.com/HeimdallS" className='content__nav-link'>
+                      <BsIcon.BsGithub className='content__nav-icon'/>
+                  </a>
+                  <a href="mailto:sham.heim@gmail.com" className='content__nav-link'>
+                      <AiIcon.AiOutlineMail className='content__nav-icon'/>
+                  </a>
+              </div>
+          </div>
+          <img src={stars} alt="star gazing" className='content__profile-pic' />
+        </div>
+        <Animation toggle={toggle}/>
+      </main>
+      <div className="content__toggle">
+            <SwitchUnstyled component={Root} onChange={handleClick} defaultChecked/> 
+        </div>
+      </>
+      
     )
 }
