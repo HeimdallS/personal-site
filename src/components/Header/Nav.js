643f8611dad071bc1d11666522d8a5746574f95f
Heimdall Sham <heim.sham@icloud.com>
2022-01-24 13:46:29 -0500

    Refactored responsive styling for homepage.

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -6,8 +6,10 @@ export default function Header() {
 
     return (
         <nav className="header">
-            <Link to='/' className='header__logo-link'><h1 className="header__logo">Heimdall Sham</h1></Link>
-            <Link to='/' className='header__logo-link'><h1 className="header__logo-tablet">HS</h1></Link>
+            <div className="header__logo-container">
+                <Link to='/' className='header__logo-link'><h1 className="header__logo">HS</h1></Link>
+                <Link to='/' className='header__logo-link'><h1 className="header__logo-tablet">Heimdall Sham</h1></Link>
+            </div>
             <ul className='header__routes'>
                <Link to='/about' className='header__link'><li className='header__route'>About</li></Link>
                 <li className='header__route'>Projects</li>
