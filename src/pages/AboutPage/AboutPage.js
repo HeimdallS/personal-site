bd1a08da8bcaaddf6e588bd60526e96f65373baf
Heimdall Sham <heim.sham@icloud.com>
2022-01-24 16:30:39 -0500

    Stlying for tablet view on about page.

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -12,21 +12,24 @@ import mysql from '../../assets/images/mysql.png';
 
 function AboutPage() {
     return (
-        <main>
-            <section className='about'>
+        <main className="about">
+            <section className='about__background-container'>
                 <h2 className="about__subhead">About</h2> 
                 <p className="about__statement">
                     After working in sales and marketing for the majority of my professional life, I consider myself 
                     an expert in communication and relationship management. However, I have always prided 
                     myself as being a life-long learner in pursuit of continued personal growth and 
-                    development. My motive in switching career paths from Sales to Web Development is to 
+                    development. 
+                    <br/>
+                    <br/>
+                    My motive in switching career paths from Sales to Web Development is to 
                     immerse myself in a constantly evolving industry; one that will allow me to utilize my 
                     communication skills to work in collaborative environments while also pushing me to 
                     challenge myself and continue to hone my programming skills and abilities.
                 </p> 
             </section>
             <section className="about__skills">
-                <h2 className="about__skills-subhead">Skills</h2>
+                <h2 className="about__subhead">Skills</h2>
                 <div className="about__stacks">
                     <div className="about__stacks-container">
                         <img src={html} alt="orange html icon" className="about__stack-icon" />
