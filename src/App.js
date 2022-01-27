b6d737e0acf668845c33aa64c9519e7781e8e9ef
Heimdall Sham <heim.sham@icloud.com>
2022-01-26 21:32:41 -0500

    Created footer component. Refactored main page styling.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import Header from './components/Header/Nav';
 import AboutPage from './pages/AboutPage/AboutPage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/about' element={<AboutPage/>} />
         </Routes>
       </>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+        <span className='footer__copyright'>Heimdall Sham © 2022</span>
+    </footer>
+  )
+  
+}
