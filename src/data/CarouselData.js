05ed63a3f594444a5a4010ad17fa679b0ddb03d1
Heimdall Sham <heim.sham@icloud.com>
2022-02-01 23:35:48 -0500

    Applied link to capstone project.

--- src/data/CarouselData.js
+++ src/data/CarouselData.js
@@ -2,6 +2,7 @@ import capstone from '../assets/images/prime-performance.png'
 import puppy from '../assets/images/puppy.jpeg'
 import kitty from '../assets/images/kitty.jpeg'
 import * as BsIcon from 'react-icons/bs';
+import * as FiIcon from 'react-icons/fi';
 
 export const CarouselData = [
     {
@@ -9,6 +10,9 @@ export const CarouselData = [
         image: `${capstone}`,
         github: <a href="https://github.com/HeimdallS/capstone-prime-performance" target="_blank" rel='noreferrer'>
         <BsIcon.BsGithub className='carousel__nav-icon'/>
+                </a>,
+        link: <a href="https://primeperformance.herokuapp.com/" target="_blank" rel='noreferrer'>
+        <FiIcon.FiExternalLink className='carousel__nav-icon'/>
                 </a>,
         title: "Prime Performance",
         description: "A full stack personal workout application for both beginners and experienced fitness enthusiasts. Utilizing React, Express, Node.js, and Sass, Prime Performance allows users to find new exercise routines, customize their own workouts, as well as generate a randomized workout for individuals who feel that their growth has plateued with their existing workout."
